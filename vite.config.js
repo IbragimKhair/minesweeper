483d9ee4b82d751cf19cce50f247c401fb478621
IbragimKhair <ibragimkhair05@gmail.com>
2025-03-31 11:48:26 +0500

    Настроен деплой на GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/minesweeper/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Указываем alias для src
+    },
+  },
 });
 
 
